30c8d0e29e801119a7ab107e55d7746c2a161202
Elwazirali <alternativewazir@hotmail.com>
2015-11-23 23:11:08 -0500

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -17,20 +17,14 @@ var changeCircle = function(){
 $(document).ready(function(){
 
 	 $(window).scroll(function () {
-                    if ($(this).scrollTop() > 550 && $(this).scrollTop()<1000 ) {
-                       $("body").css("background-image","url(http://img09.deviantart.net/ff7a/i/2015/123/8/a/flat_landscape_by_ben_aka_riot-d8s19au.png)")
-					   $("body").css("background-attachment","fixed");
-					   $('body').addClass('hey')
-					   
-					}
-					else if($(this).scrollTop() < 400)
+                    					if($(this).scrollTop() < 400)
 						{
 							$('body').css("background","url(http://i.imgur.com/kk1ZaZr.jpg)")
 							$('body').css("background-attachment","fixed")
 							$('body').css("background-size","100% 100%")
 							
 						}
-						else if($(this).scrollTop()>=1000)
+						else if($(this).scrollTop()>=1000 )
 						{
 							$('body').css("background","url(http://i.imgur.com/q8YiK1v.jpg)")
 							$('body').addClass('hey')
